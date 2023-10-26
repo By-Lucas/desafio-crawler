7cb3406522e4cee4cca806bf4d83e930d85f94c7
By-Lucas <tekertudo@gmail.com>
2023-10-26 14:31:30 -0300

    Edit templates and create models

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from helpers.base_models import BaseModelTimestamp
+
+
+class NotificationsModel(BaseModelTimestamp):
+    title = models.CharField(verbose_name=_("Título"), max_length=150, null=True, blank=True)
+    description = models.CharField(verbose_name=_("Descrição"), max_length=150, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.title if self.title else str(self.id)
+    
+    class Meta:
+        verbose_name = _("Noticação")
+        verbose_name_plural = _("Noticações")
--- apps/data_scrapy/models.py
+++ apps/data_scrapy/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from helpers.base_models import BaseModelTimestamp
+
+
+class ScrapyQuotesModel(BaseModelTimestamp):
+    title = models.CharField(verbose_name=_("Título"), max_length=150, null=True, blank=True)
+    author = models.CharField(verbose_name=_("Autor"), max_length=150, null=True, blank=True)
+    tags = models.CharField(verbose_name=_("Autor"), max_length=1000, null=True, blank=True)
+    description = models.TextField(verbose_name=_("Descrição"), null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.title if self.title else str(self.id)
+    
+    class Meta:
+        verbose_name = _("Scrapy Quotes")
+        verbose_name_plural = _("Scrapy Quotes")
